20a4fb3195c62f56eba7bb27608de4166774895c
Brian <brian0915.cathay@gmail.com>
2024-08-03 15:20:39 +0800

    branch fix

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,6 @@
 public class Calculator {
-    public int add(int a, int b) {
-        return a + b;
+    public int add(int a, int b, int c) {
+        return a + b + c;
     }
 
     public int subtract(int a, int b) {
