b4c47ffe22f6d439f8e437297c7059c9aefdad8b
KuoJingChiang <brian0915.cathay@gmail.com>
2024-08-04 10:26:47 +0800

    new pull request

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,7 @@
 public class Calculator {
 
-    public int add(int a, int b, int c) {
-        return a + b + c;
+    public int add(int a, int b, int c, int d) {
+        return a + b + c + d;
 
     }
 
